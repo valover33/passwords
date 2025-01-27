1541e749066dcb4d8affc003d83fe06fdb504883
Valerii <valover@mail.ru>
2025-01-27 17:40:27 +0300

    Доп. задание по модулю 2

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ def main():
     nat_num_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
     rand_num = random.choice(rand_num_list)
     for i in nat_num_list:
-        if i < rand_num:
-            for j in nat_num_list:
+        if i <= rand_num // 2:
+            for j in  nat_num_list:
                 if (rand_num % (i + j)) == 0 and i < j:
                     result = result + "%s%s" % (i, j)
                     if (i + j) > rand_num:
